56fa25c5463370c631f377e590f0a3cbfc628435
kuniquoc <102220122@sv1.dut.udn.vn>
2025-06-13 09:46:26 +0700

    feat: update MobileNetV2_Crying model to enhance input channel handling and clarify comments for better understanding

--- src/models/cnn_model.py
+++ src/models/cnn_model.py
@@ -11,16 +11,18 @@ class MobileNetV2_Crying(nn.Module):
         # Chuyển đầu vào thành 1 kênh (MFCC)
         self.model.features[0][0] = nn.Conv2d(
             in_channels=1,      # MFCC có 1 kênh
-            out_channels=32,
-            kernel_size=3,
-            stride=2,
-            padding=1,
-            bias=False
+
+            # Giữ nguyên các thông số khác để đảm bảo tính tương thích với mô hình gốc
+            out_channels=32,    # Số lượng kênh đầu ra giữ nguyên theo mô hình gốc để đầu ra phù hợp với đầu vào của lớp tiếp theo
+            kernel_size=3,      # kích thước kernel giữ nguyên
+            stride=2,           # nhảy 2 pixel để giảm lượng tính toán
+            padding=1,          # padding để tranh mất thông tin biên
+            bias=False          # Không sử dụng bias vì đã có BatchNorm ở lớp tiếp theo trong mô hình gốc
         )
 
         # Thay đổi đầu ra: 1000 lớp → 1 lớp nhị phân
         self.model.classifier = nn.Sequential(
-            nn.Dropout(0.2), #nên dùng dropout để tránh overfitting, thử nghiệm với 0.5
+            nn.Dropout(0.2), # tránh overfitting
             nn.Linear(self.model.last_channel, 1)  # Binary classification
         )
 
